e326d2e84423bc0e38ef8bc8410b6dba14c8cc70
Matija Moslavac <moslavac.matija@gmail.com>
2018-06-14 17:34:01 +0200

    Fixing and adding all

---
+++ README.md
@@ -0,0 +1,12 @@
+Arcade Game
+
+
+HOW TO PLAY:
+
+Use the arrow keys to move your player up across the grass and the stones to the water without getting hit by a bug.
+You get THREE LIVES to start with.
+Each time you reach the water, you earn an extra life.
+You get 200 POINTS whenever you reach the water and 100 POINTS for each treasure you pick up along the way.
+When you clear off all the treasures, more will appear!
+
+Your player image is chosen at random from the five images at the start of the game. You may change your image at any time by clicking another image.
---
+++ js/app.js
@@ -0,0 +1,321 @@
+/**
+ * starting and reset point for the player
+ * @constant
+ * @todo remove from global scope  
+ */
+var START_X = 203;
+var START_Y = 396;
+
+/**
+ * a frequently-used formula for picking n random numbers
+ * @function
+ */
+var randNum = function(n) {
+    return Math.floor(Math.random() * n);
+};
+
+/**
+ * variables to keep track of lives remaining, score and high score
+ * @todo remove from global scope
+ */
+var lives = 3;
+var score = 0;
+var highScore = 0;
+
+/**
+ * a function to put lives, score, high score on the screen (in the DOM)
+ * @function
+ */
+var scoring = function() {
+    document.getElementById("score_box").innerHTML =
+        "Score: <strong>" + score + "</strong>   ";
+    document.getElementById("lives_remaining").innerHTML =
+        "Lives Remaining: <strong>" + lives + "</strong>    ";
+    document.getElementById("high_score").innerHTML =
+        "Your High Score: <strong>" + highScore + "</strong>";
+};
+
+/**calls the function when the game loads*/
+scoring();
+
+/**
+ * five possible player images
+ * @todo remove from global scope
+ */
+var playerImages = ['images/char-boy.png',
+    'images/char-horn-girl.png',
+    'images/char-pink-girl.png',
+    'images/char-princess-girl.png',
+    'images/char-cat-girl.png'
+];
+
+/**add all player images to the html and add a listener for clicks...
+ * which allows user to change player image at any time
+ */
+document.getElementById("sprite1").addEventListener("click", function() {
+    player.sprite = playerImages[0];
+});
+document.getElementById("sprite2").addEventListener("click", function() {
+    player.sprite = playerImages[1];
+});
+document.getElementById("sprite3").addEventListener("click", function() {
+    player.sprite = playerImages[2];
+});
+document.getElementById("sprite4").addEventListener("click", function() {
+    player.sprite = playerImages[3];
+});
+document.getElementById("sprite5").addEventListener("click", function() {
+    player.sprite = playerImages[4];
+});
+
+/** 
+ * Enemies our player must avoid
+ * @constructor
+ */
+function Enemy(sprite, x, y, speed) {
+    /** uses a helper we've provided to easily load images*/
+    this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+}
+
+/** Update the enemy's position, required method for game
+ *  Parameter: dt, a time delta between ticks
+ */
+Enemy.prototype.update = function(dt) {
+    /** You should multiply any movement by the dt parameter
+     * which will ensure the game runs at the same speed for
+     * all computers. */
+    this.x += this.speed * dt;
+	var offTheBoard = 500;
+    if (this.x > offTheBoard) {
+        this.reset();
+    }
+};
+
+/** Send enemies back to a random spot with a negative x value
+ * and a new random speed */
+Enemy.prototype.reset = function() {
+    this.x = ((Math.random() * -900) - 100);
+    this.speed = (Math.random() * 400) + 200;
+};
+
+/** Draw the enemy on the screen, required method for game */
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+/** 
+ * Creates the Player class of objects
+ * @constructor
+ */
+function Player(sprite, x, y) {
+    //pick one of the five player sprites at random to start the game
+    this.sprite = playerImages[randNum(5)];
+    //position the sprite at the start point
+    this.x = START_X;
+    this.y = START_Y;
+}
+
+Player.prototype.reset = function() {
+    //reset puts player back to the start, updates score and lives...
+    this.x = START_X;
+    this.y = START_Y;
+    //...and checks to see if any lives are left
+    if (lives === 0) {
+        //stops the game with an alert, reports the score
+        alert("Game Over! Score: " + score);
+        //updates the high score if appropriate
+        if (score > highScore) {
+            highScore = score;
+        }
+        //resets score, lives for new game
+        score = 0;
+        lives = 3;
+        //calls fresh treasure and enemy functions
+        newTreasure();
+        newEnemy();
+    }
+    //...and calls scoring function to update score and lives
+    scoring();
+};
+
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    //values chosen to keep player roughly centered in the squares
+
+    this.checkCollisions();
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//The following controls the player's movement
+Player.prototype.handleInput = function(input, allowedKeys) {
+    var VERTICAL_MOVE = 83;
+    var HORIZONTAL_MOVE = 100;
+	/**
+	 * Boundaries of the game board (x or y values)
+	 * @constant
+	 */
+	var LEFT_WALL = 3;
+	var RIGHT_WALL = 403;
+	var BOTTOM_WALL = 396;
+	var WATER = 63;
+	switch (input) {
+        case 'up':
+            this.y -= VERTICAL_MOVE;
+            /* if player reaches the water, add 200 to score, add a life,
+            update onscreen score/lives, send player back to start. */
+            if (this.y <= WATER) {
+                score += 200;
+                lives += 1;
+                this.reset();
+                scoring();
+            }
+            break;
+        case 'down':
+            //moves player down unless player is at bottom wall
+            if (this.y < BOTTOM_WALL) {
+                this.y += VERTICAL_MOVE;
+            }
+            break;
+        case 'left':
+            // moves player left unless at left wall
+            if (this.x > LEFT_WALL) {
+                this.x -= HORIZONTAL_MOVE;
+            }
+            break;
+        case 'right':
+            //moves player right unless at right wall
+            if (this.x < RIGHT_WALL) {
+                this.x += HORIZONTAL_MOVE;
+            }
+            break;
+    }
+};
+
+//made this part of Player prototype rather than Enemy since player collision
+//must be checked against enemies AND treasure
+Player.prototype.checkCollisions = function() {
+    //check player-enemy collisions
+    //loop through each enemy...
+    for (i = 0; i < allEnemies.length; i++) {
+        //...see if y values match exactly and x values are within 80 in either
+        //direction. the value 80 eliminates collisions of transparent margins
+        if (this.y == allEnemies[i].y && Math.abs(this.x -
+                allEnemies[i].x) < 80) {
+            //removes a life and send player back to start
+            lives -= 1;
+            this.reset();
+        }
+    }
+    //check player-treasure collisions
+    //loop through each treasure...
+    for (i = 0; i < allTreasures.length; i++) {
+        //...see if x and y values match
+        if (this.x == allTreasures[i].x && this.y == allTreasures[i].y) {
+            //remove the treasures from the board's visible area
+            allTreasures[i].x = -300;
+            allTreasures[i].y = 396;
+            //remove the treasure from the allTreasures array
+            allTreasures.splice([i], 1);
+            //if all the treasures are gone, respawn them!
+            if (allTreasures.length === 0) {
+                newTreasure();
+            }
+            //award the player 100 points
+            score += 100;
+            //update the scoring displayed on screen
+            scoring();
+        }
+    }
+};
+
+/** 
+ * Creates the Treasure class of objects
+ * @constructor
+ */
+function Treasure(sprite, x, y) {
+    this.sprite = sprite;
+    this.x = x;
+    this.y = y;
+}
+
+Treasure.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
+var allEnemies = [];
+//loop thru this function pushing six enemies to allEnemies
+var newEnemy = function() {
+	//'use strict';
+    //empty out allEnemies by setting length to zero. 
+    //to create a new set of enemies with each new game
+    allEnemies.length = 0;
+	//x[0] varies start point from just off the board to about halfway
+	//across the board, and x[1] varies placement off (to the left) 
+	//of the board
+	x = [((Math.random() - 0.2) * 450), ((Math.random() * -500) - 200)];
+	//y values chosen to center enemies vertically on path
+	y = [64, 147, 230];
+	//provides a variety of speeds
+	speed = ((Math.random() * 600) + 200);
+	//choose x[0] or x[1] and y[0], y[1] or y[2]
+    for (i = 0; i <= 5; i++) {
+        var enemy = new Enemy(this.sprite, x[randNum(2)], y[randNum(3)], speed);
+        allEnemies.push(enemy);
+    }
+};
+
+newEnemy();
+
+var player = new Player();
+
+var allTreasures = [];
+//loop through this function, push one, two or three treasures to allTreasures
+var newTreasure = function() {
+    //empty out allTreasures by setting length to zero. 
+    //to create a new set of treasures with each new game
+    allTreasures.length = 0;
+    //images to be used for treasure
+    var treasureSprite = ['images/Key.png',
+        'images/Star.png',
+        'images/Gem Blue.png',
+        'images/Gem Orange.png',
+        'images/Heart.png'
+    ];
+    //boundaries of the 15-square treasure area (the gray stone squares)
+    var treasureX = [3, 103, 203, 303, 403];
+    var treasureY = [230, 147, 64];
+    //run loop at least twice and as many as five times
+    for (i = 0; i <= (randNum(4) + 1); i++) {
+        //chose randomly among the treasure sprites
+        var treasure = new Treasure(treasureSprite[randNum(5)],
+            //place treasures from left to right w/unique x, random y
+            treasureX[i], treasureY[randNum(3)]);
+        allTreasures.push(treasure);
+    }
+};
+
+newTreasure();
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+    player.handleInput(allowedKeys[e.keyCode]);
+});
